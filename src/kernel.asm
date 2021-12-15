98557593938dd8e8114a21d937c1ba95d7733bb7
Larry <larrythwu@gmail.com>
2021-12-15 00:56:33 -0500

    Added code in boot.asm that will load the kernel sectors from harddrive into memory at 0x10000 (1MB) position and start executing

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,8 +1,11 @@
+[BITS 32]; all code after this is seem as 32 bit code 
+global _start ;export the sympol
+
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
 
-[BITS 32]; all code after this is seem as 32 bit code 
-load32: ;set up the 32 bit registers
+
+_start: ;set up the 32 bit registers
     mov ax, DATA_SEG
     mov ds, ax
     mov es, ax
